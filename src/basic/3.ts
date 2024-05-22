149b2bebd593a586cc035eb110609365a2a8bf04
Angelina <midnightshout03@gmail.com>
2024-05-22 23:01:11 +0300

    task 3

---
+++ src/basic/3.ts
@@ -0,0 +1,11 @@
+// UNION TYPE
+let unionType: string | number;
+unionType = "test";
+unionType = 100;
+
+// LITERAL TYPE
+type toggle = 'enable' | 'disable';
+let value: toggle;
+value = 'enable';
+value = 'disable';
+
